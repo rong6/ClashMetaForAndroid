19bb5f7dd37315a5ad12ed9ae50ba9c05a3abcdc
rong6 <109727098+rong6@users.noreply.github.com>
2026-02-09 08:21:50 +0000

    fix: 修复keystore错误

--- build.gradle.kts
+++ build.gradle.kts
@@ -151,7 +151,7 @@ subprojects {
                         keystore.inputStream().use(this::load)
                     }
 
-                    storeFile = rootProject.file("release.keystore")
+                    storeFile = rootProject.file("keystore.jks")
                     storePassword = prop.getProperty("keystore.password")!!
                     keyAlias = prop.getProperty("key.alias")!!
                     keyPassword = prop.getProperty("key.password")!!
